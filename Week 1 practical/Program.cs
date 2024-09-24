20812757cd1eabd400c88e83365eaa45a564daaf
Oisin Henry <Henry-O1@ulster.ac.uk>
2024-09-24 15:58:33 +0100

    more changes to program.cs file

--- Week 1 practical/Program.cs
+++ Week 1 practical/Program.cs
@@ -15,8 +15,16 @@
     {
         Console.WriteLine("Please enter your age?");
         int age = Convert.ToInt32(Console.ReadLine());
+        if (age > 13)
+        {
+            Console.WriteLine("You are" + " " + age + "years old, you are a child");
+        }
+        else if (age >= 13 && age <= 18) 
+        {
+            Console.WriteLine("You are" + " " + age + "years old, you are a teenager");
 
+        }
+        else if (age >= 19 &&  110) // cant get this line to work
+        {
 
-
-    }
 }
