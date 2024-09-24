2670435194a9b9fad01f85c17c046c65a22cea6b
csf23heo <csf23heo@MF124PC39>
2024-09-24 15:26:07 +0100

    Add project files.

---
+++ Week 1 practical/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Please enter your name:");
+string name = Console.ReadLine();
+Console.WriteLine("Please enter your age?");
+int age = Convert.ToInt32(Console.ReadLine());
+int newage = (age + 5);
+Console.WriteLine("Hello" + " " + name + "," + " " + "you will be" + " " + newage + " " + ("in 5 years"));
+
+
