575c5daf9fc74c04317b21800ba6601460edc4b0
Oisin Henry <Henry-O1@ulster.ac.uk>
2024-09-24 15:44:11 +0100

    Changed alot in program.cs file

--- Week 1 practical/Program.cs
+++ Week 1 practical/Program.cs
@@ -1,8 +1,22 @@
-﻿Console.WriteLine("Please enter your name:");
-string name = Console.ReadLine();
-Console.WriteLine("Please enter your age?");
-int age = Convert.ToInt32(Console.ReadLine());
-int newage = (age + 5);
-Console.WriteLine("Hello" + " " + name + "," + " " + "you will be" + " " + newage + " " + ("in 5 years"));
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+       // Console.WriteLine("Please enter your name:");
+       // string name = Console.ReadLine();
+       // Console.WriteLine("Please enter your age?");
+       // int age = Convert.ToInt32(Console.ReadLine());
+       // int newage = age + 5;
+        // Console.WriteLine("Hello" + " " + name + "," + " " + "you will be" + " " + newage + " " + "in 5 years");
+    }
 
 
+    public static int Agecheck(string[] args)
+    {
+        Console.WriteLine("Please enter your age?");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+
+
+    }
+}
